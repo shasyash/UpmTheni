c331aeb14f262975115d1a22a3d9a36920cdbb32
shasyash <mdyasarafath2000@gmail.com>
2025-01-06 17:25:39 +0530

    project created and project

---
+++ src/test/java/com/cumbum/GitPratice.java
@@ -0,0 +1,17 @@
+package com.cumbum;
+
+
+
+public class GitPratice {
+
+	
+	private void method1() {
+		System.out.println("1");
+	}
+	private void Method2() {
+		System.out.println("2");
+	}
+	private void method3() {
+		System.out.println("3");
+	}
+}
